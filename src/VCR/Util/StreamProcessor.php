154396d6b75eac805f84d5a0e80d5bfcae77de75
Robert Pustułka <robert.pustulka@gmail.com>
2021-05-13 12:23:59 +0200

    Set $this->isIntercepting to false on restore()

--- src/VCR/Util/StreamProcessor.php
+++ src/VCR/Util/StreamProcessor.php
@@ -85,6 +85,8 @@ class StreamProcessor
         // stream_wrapper_restore can throw when stream_wrapper was never changed, so we unregister first
         stream_wrapper_unregister(self::PROTOCOL);
         stream_wrapper_restore(self::PROTOCOL);
+
+        $this->isIntercepting = false;
     }
 
     /**
